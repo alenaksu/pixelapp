3ed3af263a87d145037947c217bf75c12661ec0c
Alessandro Valeri (alenaksu) <misterroot@gmail.com>
2020-09-20 23:21:04 +0200

    chore: fix github pages build

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,22 @@
 module.exports = {
+    mount: {
+        'src': '/static'
+    },
     extends: '@snowpack/app-scripts-lit-element',
     plugins: [
         './tools/glslPlugin.js',
         './tools/cssImportTextPlugin.js',
         '@snowpack/plugin-optimize',
     ],
+    devOptions: {
+        out: './docs',
+    },
+    installOptions: {
+        sourceMap: false,
+        treeshake: true,
+    },
+    buildOptions: {
+        baseUrl: '/pixelapp/',
+        clean: true
+    }
 };
